d80ff164e99d91f7379ca559db8761a56d3d6bd2
shubham singh Rathore <138260347+NandSingh007@users.noreply.github.com>
2024-08-27 12:07:07 +0530

    sckript

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mt-24">
+      <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+        responsive dashboard page in ReactJS featuring interactive charts and
+        tables to visualize analytical data
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
